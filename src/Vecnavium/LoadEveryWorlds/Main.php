5c91d5f194b42d7852b815652d2bd78f4f6c7e00
Vecnavium <83182010+Vecnavium@users.noreply.github.com>
2021-09-12 12:40:35 -0700

    Re-published

---
+++ src/Vecnavium/LoadEveryWorlds/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Vecnavium\LoadEveryWorlds;
+
+
+use pocketmine\plugin\PluginBase;
+use function array_diff;
+use pocketmine\world\WorldManager;
+use function scandir;
+
+
+class Main extends PluginBase {
+
+    public function onLoad(): void {
+		foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), [".."]) as $AllWorlds){
+
+			if($this->getServer()->getWorldManager()->loadWorld($AllWorlds)){
+
+                $this->getLogger()->info("§eWorld ${AllWorlds} has been loaded");
+			}
+		}
+	}
+}
