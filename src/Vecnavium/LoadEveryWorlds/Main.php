b1758475b54762986f2e559f5a5bf1936dc1dbd4
Vecnavium <83182010+Vecnavium@users.noreply.github.com>
2021-12-12 09:16:40 -0800

    Reverted all changes (it can get approved)

--- src/Vecnavium/LoadEveryWorlds/Main.php
+++ src/Vecnavium/LoadEveryWorlds/Main.php
@@ -13,8 +13,8 @@ class Main extends PluginBase {
 
     public function onLoad(): void {
 		foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), [".."]) as $AllWorlds){
-						if($this->getServer()->getWorldManager()->loadWorld($AllWorlds)){
-			}
+			if($this->getServer()->getWorldManager()->loadWorld($AllWorlds)){
+                        $this->getLogger()->info("§eWorld ${AllWorlds} has been loaded");
 		}
 	}
 }
